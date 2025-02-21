4ffb79c32b832b334365650443b1e993525f6a59
Kamalesh Devaraj <KDevaraj@careconnectmd.com>
2025-02-21 20:56:09 +0530

    New Dashboard Layout Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [SidebarComponent, TopbarComponent, RouterOutlet],
 })
 export class AppComponent {}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  menuItems = [
+    { name: 'Summary', path: '/summary' },
+    { name: 'Transactions', path: '/transactions' },
+    { name: 'Mandates', path: '/mandates' },
+    { name: 'Loans', path: '/loans' },
+    { name: 'Investments', path: '/investments' },
+    { name: 'Analytics', path: '/analytics' },
+    { name: 'Settings', path: '/settings' },
+    { name: 'Help', path: '/help' },
+  ];
+}
