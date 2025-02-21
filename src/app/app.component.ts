47285dad7361d84588d544e3e269162b01770f05
Kamalesh Devaraj <KDevaraj@careconnectmd.com>
2025-02-21 18:06:41 +0530

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { DailydayComponent } from './components/dailyday/dailyday.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [CalendarComponent, DailydayComponent, NavbarComponent],
 })
-export class AppComponent {
-  selectedDate = signal<string | null>(null);
-  selectedMonth = signal<string | null>(null);
-  selectedYear = signal<string | null>(null);
-
-  onDateSelected(date: string) {
-    this.selectedDate.set(date);
-  }
-
-  onMonthSelected(month: string) {
-    this.selectedMonth.set(month);
-  }
-
-  onYearSelected(year: string) {
-    this.selectedYear.set(year);
-  }
-}
+export class AppComponent {}
