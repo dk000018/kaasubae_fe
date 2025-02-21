0f3f3ef71da188e7f5aae3a72778f5cafdf54408
Kamalesh Devaraj <KDevaraj@careconnectmd.com>
2025-02-21 23:06:13 +0530

    Layout Routes Active Highlighter Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { SummaryComponent } from './pages/summary/summary.component';
 
 export const routes: Routes = [
   { path: 'settings', component: SettingsComponent },
+  { path: '', component: SummaryComponent },
 ];
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
   menuItems = [
-    { name: 'Summary', path: '/summary' },
-    { name: 'Transactions', path: '/transactions' },
-    { name: 'Mandates', path: '/mandates' },
-    { name: 'Loans', path: '/loans' },
-    { name: 'Investments', path: '/investments' },
-    { name: 'Analytics', path: '/analytics' },
-    { name: 'Settings', path: '/settings' },
-    { name: 'Help', path: '/help' },
+    { name: 'Summary', path: '' },
+    { name: 'Transactions', path: 'transactions' },
+    { name: 'Mandates', path: 'mandates' },
+    { name: 'Loans', path: 'loans' },
+    { name: 'Investments', path: 'investments' },
+    { name: 'Analytics', path: 'analytics' },
+    { name: 'Settings', path: 'settings' },
+    { name: 'Help', path: 'help' },
   ];
 }
