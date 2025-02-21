7c2b42e5e0e4009d1c0344cb08c3fa88dbb67032
Kamalesh Devaraj <KDevaraj@careconnectmd.com>
2025-02-21 18:33:38 +0530

    New Schema Created

---
+++ src/app/pages/analytics/analytics.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics',
+  imports: [],
+  templateUrl: './analytics.component.html',
+})
+export class AnalyticsComponent {}
